7adf41ab06f6b5d31d640f82462976295b4961d7
Alexander Bates <alexanderbates51@gmail.com>
2025-06-27 10:44:50 -0500

    Added the final versions of the student data api and app folders.
    MSA has perhaps been the most dense period of skill development in my life thus far. Thank you so much to Kristofferson Culmer (and Sabrina for Tiny Sparks lmao). I can not wait to continue to progress and then come back to this and laugh at it. - Alexander J.T. Bates

--- student_web_app/app.py
+++ student_web_app/app.py
@@ -27,6 +27,50 @@ def index():
      # Make a requst to the student data api
      url = 'http://127.0.0.1:5000/api/students/all'
      student_data = get_student_data(url)
-     return render_template('index.html')
+     return render_template('index.html', student_data=student_data)
+
+# Create a route for the majors search page
+@app.route('/major', methods=['GET'])
+def majors_get():
+     # Get a list of student data
+     url = 'http://127.0.0.1:5000/api/students/all'
+     student_data = get_student_data(url)
+     # Create a list to store unique majors
+     major_list = []
+     # Use a for loop to interate through the student list
+     for student in student_data:
+          # Add major to majors list if major not currently in list
+          if student['major'] not in major_list:
+               major_list.append(student['major'])
+     # Sort the major list
+     major_list.sort()
+     return render_template('majors.html', major_list=major_list)
+
+# Create a route for the majors search page
+@app.route('/major', methods=['POST'])
+def majors_post ():
+     # Get a list of student data
+     url = 'http://127.0.0.1:5000/api/students/all'
+     student_data = get_student_data(url)
+     # Create a list to store unique majors
+     major_list = []
+     # Use a for loop to interate through the student list
+     for student in student_data:
+          # Add major to majors list if major not currently in list
+          if student['major'] not in major_list:
+               major_list.append(student['major'])
+
+     # Sort the major list
+     major_list.sort()
+     # Get the form data
+     major = request.form.get('major')
+     print(f"This is majors{major}")
+     # Create the request url to get students with that major
+     url = f"http://127.0.0.1:5000/api/major/{major}" 
+     # Send the request and get the response 
+     result_list = get_student_data(url)
+     # Send the results to the majors template
+     return render_template('majors.html', major_list=major_list, result_list=result_list)
+
 # Run the flask application
 app.run(port=5001)
